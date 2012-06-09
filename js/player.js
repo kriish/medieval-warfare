4e365921228662b742ee2b06bdc25abd29396e8c
Shaohong Li <shaohong.li@aalto.fi>
2012-06-09 13:30:22 +0300

    player and some initial backend stuff

--- js/player.js
+++ js/player.js
@@ -1,17 +1,54 @@
-// player name also acts as a globally unique id
-var playerName = false;
 
-// hash by player name, stores pointer to players Kingdom
-var players = false;
 
-function Kingdom(player) {
-var troops = false;
-var cities = false;
+//player_id
+//cities 
+//	city.name
+//	city.location
+//
+//troops
+//    troop.name
+//    troop.location
+//    size #how many people
+//    
+//money 
+//submitted round
 
-    function addCity(city) {
-       cities[city.name] = city;
-    }
-    function addTroop(troop) {
-       troops[troop.name] = troop;
-    }
+function player( player_id, initialMoney ) {
+	this.id = player_id;
+	
+	// cities:
+	this.cities = new Object();
+	
+	// troops:
+	this.troops = new Object();
+
+	// money:
+	this.money = initialMoney;
+	
+	// submittedRount
+	this.submittedRound = 0;
+	
+	this.increamentSubmittedRount = function () {
+		this.submittedRound ++;
+	};
+	
+	this.addMoney = function(someAmount) {
+		this.money += someAmount;
+	};
+	
+	this.substractMoney = function (someAmount) {
+		this.money -= someAmount;
+	};
+
+	// create a city
+	this.createCity = function (theCity) {
+	    console.log("createCity [" + theCity.name + ", " + theCity.location);
+	    this.cities[theCity.name] = theCity;
+	};
+	
+	// destroy a City
+	this.destroyCity = function (theCity) {
+	    console.log("destroyCity [" + theCity.name + ", " + theCity.location);
+		delete this.cities[theCity.name];
+	};
 }
